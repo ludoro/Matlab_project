a8e89c57c21c1302da6d6d97906935259cfc6c34
lucacat97 <38509462+lucacat97@users.noreply.github.com>
2018-04-27 11:50:52 +0200

    Add files via upload
    Non funziona la rappresentazione della frattura

---
+++ 3D/show_bozza.m
@@ -0,0 +1,28 @@
+
+grid on
+
+for i=1:n_edges
+    plot3([node(edge(i,1)).x node(edge(i,2)).x ],...
+          [node(edge(i,1)).y node(edge(i,2)).y ],...
+          [node(edge(i,1)).z node(edge(i,2)).z ],...
+          '.-b','LineWidth',1.3);
+      hold on
+end 
+
+for id_n=1:n_nodes
+X(id_n,:) = [node(id_n).x node(id_n).y node(id_n).z];
+end
+tetramesh(tets,X,[1:n_nodes]) ;
+axis equal
+
+for i=1:n_fractures
+    j=1;
+    while j<length(fract(i))
+    plot3( [fract_vertex(fract(i,j),1) fract_vertex(fract(i,j+1),1)],...
+          [fract_vertex(fract(i,j),2) fract_vertex(fract(i,j+1),2)],...
+          [fract_vertex(fract(i,j),3) fract_vertex(fract(i,j+1),3)],'.b' )
+          hold on
+    j=j+1;
+    end
+end
+clear j; 
